ab8a076dd560c7321d595739cab5a52b0c515eb5
Dawson Ley <anarchonist@protonmail.com>
2018-11-06 20:07:44 +0000

    character counter refactored and functioning as efficient as can be, implementing hardcoded tweet currently

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,18 +3,22 @@ var count = 140;
 $(document).ready(function() {
   console.log('I am being loaded into the index.html file directly!');
   $('#countMe').on('keyup', function() {
-
-    let current = $('#textPlace').val().length;
-    console.log(current);
-    let chars = count - current;
-    $('.counter').html(chars);
-
-    if (current > 140) {
-      $('.counter').css('color', 'red');
+    let current = $('textarea', this).val().length;
+    // var $counter = $('span', this).html(count - current);
+    var spanColor = 'black';
+    if(current >= 140){
+      spanColor = 'red';
     }
 
-    if (current <= 140) {
-      $('.counter').css('color', 'black');
-    }
+    $('span', this)
+      .html(count - current)
+      .css('color', spanColor);
+    // if (current > 140) {
+    //   $('span', this).css('color', 'red');
+    // }
+
+    // if (current <= 140) {
+    //   $('span', this).css('color', 'black');
+    // }
   });
 });
